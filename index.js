e7769ec1fe7b0a234ded46757f4f0a2a98f442ad
Eunice Umutoni <136147297+euniceumutoni@users.noreply.github.com>
2024-01-23 21:36:51 -0600

    index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+
+var randomNumber = Math.floor(Math.random() * 6) +1;
+document.getElementById("randomNumber").innerHTML = randomNumber;
+document.querySelector(".dice .img1").setAttribute("src", `C:...path...images\dice${randomNumber1}.png`);
