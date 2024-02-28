6f4c1dbdbc298ab0a541f57016bee4924b719d06
Eunice Umutoni <136147297+euniceumutoni@users.noreply.github.com>
2024-02-28 08:08:40 -0600

    index.js

--- index.js
+++ index.js
@@ -9,11 +9,17 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
+
+
+//for the second random number
 var randomNumber2  = Math.floor(Math.random() * 6) + 1;
 
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+
 
 if (randomNumber1 > randomNumber2) {
     
